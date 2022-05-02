682b854cc4fd903eabe604656c65dbe8f3e5c053
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2022-05-02 22:25:53 +0530

    Add files via upload
    commit

---
+++ PrimitiveStringCompare.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class PrimitiveStringCompare {
+	public static void main(String args[]) {
+		if(new String("sunil")==new String("harish")) { // primitive type to create string 
+			System.out.println("String are Equals.");
+		}
+		else {
+			System.out.println("String are not Equal.");
+		}
+	}
+
+}
