575f943d85b2a7609df134331cde4601b6870b2c
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2022-05-02 22:04:37 +0530

    Add files via upload
    commit

---
+++ ConcatinationString.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ConcatinationString {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the first Name:");
+		String firstName=sc.next();
+		System.out.println("Enter the Last Name:");
+		String lastname=sc.next();
+		String Fullname=firstName+lastname;
+		System.out.println("the concatination of string is:"+Fullname);
+		
+	}
+
+}
