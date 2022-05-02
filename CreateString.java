36f72969ef719466f0c5dca5ab26216850d16b3a
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2022-05-02 21:40:22 +0530

    Add files via upload
    commit

---
+++ CreateString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CreateString {
+public static void main(String args[]) {
+	String name1="Harish";
+	String name2="Sunil";
+	System.out.println("My name is:"+name1);
+	System.out.println("my name is:"+name2);
+	String sentence="I love My family most in world";
+	System.out.println(sentence);
+}
+}
