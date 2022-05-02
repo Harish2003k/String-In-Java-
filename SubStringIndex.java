57c6955feffa9ecf89f27b3dc02f39ece99a8172
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2022-05-02 22:58:52 +0530

    Add files via upload
    commit

---
+++ SubStringIndex.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class SubStringIndex {
+	public static void main(String args[]) {
+		String sentence="My Name is Arti Gupta";
+		//String name =sentence.substring(3); // first way 
+		String name=sentence.substring(5,sentence.length());
+		
+		System.out.println(name);
+	}
+
+}
