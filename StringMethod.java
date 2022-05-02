b8df4ee548206f9fd3405d86ee2713694976cb29
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2022-05-02 23:06:58 +0530

    Add files via upload
    commit

---
+++ StringMethod.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class StringMethod {
+public static void main(String args[]) {
+  int number =1234;
+	String str1 =Integer.toString(number);
+	System.out.println(str1.length());
+}
+}
