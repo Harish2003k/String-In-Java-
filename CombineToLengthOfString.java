ce0b5509062a7a5c28410ac33e09eb7630ae522d
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2022-05-03 16:28:42 +0530

    Add files via upload
    commit

---
+++ CombineToLengthOfString.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class CombineToLengthOfString {
+public static void main(String args[]) {
+	Scanner sc=new Scanner(System.in);
+	int size=sc.nextInt();
+	String []array=new String[size];
+	int toLength=0;
+	for(int i=0;i<size;i++) {
+		array[i]=sc.next();
+		toLength=toLength+array[i].length();
+	}
+	System.out.println(toLength);
+}
+}
